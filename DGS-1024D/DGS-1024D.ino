393dea8e718e5db543d8c3e30158f6dcf8e7649f
Alberto Panu <alberto@panu.it>
2016-11-02 15:38:17 +0100

    Update DGS-1024D.ino

--- DGS-1024D/DGS-1024D.ino
+++ DGS-1024D/DGS-1024D.ino
@@ -8,7 +8,8 @@
 //          : of the D-Link DGS-1024D switch
 //****************************************************************
 
-//Pin connected to 74HC164 led board
+/* Old config
+// Pin connected to 74HC164 led board
 #define clockPin5e6 12 // pin 5 DGS-1024D CP5E6
 #define clockPin3e4 10 // pin 9 DGS-1024D CP3E4
 #define clockPin1e2 8 // pin 13 DGS-1024D CP1E2
@@ -16,6 +17,17 @@
 #define dataPin5e6 11 // pin 7 DGS-1024D DS5
 #define dataPin3e4 9 // pin 11 DGS-1024D DS3
 #define dataPin1e2 7 // pin 15 DGS-1024D DS1
+*/
+
+// Pin connected to 74HC164 led board new config better
+// with original cable and Arduino Nano
+#define clockPin5e6 7 // pin 5 DGS-1024D CP5E6
+#define clockPin3e4 9 // pin 9 DGS-1024D CP3E4
+#define clockPin1e2 11 // pin 13 DGS-1024D CP1E2
+////Pin connected to DS of 74HC164
+#define dataPin5e6 8 // pin 7 DGS-1024D DS5
+#define dataPin3e4 10 // pin 11 DGS-1024D DS3
+#define dataPin1e2 12 // pin 15 DGS-1024D DS1
 
 //#define DEBUG
 
