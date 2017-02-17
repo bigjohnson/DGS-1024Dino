7884a52e05c719924be56a45518a1345dddb5fea
Alberto Panu <alberto@panu.it>
2017-02-17 14:25:25 +0100

    Added Arduino Mini pin

--- DGS-1024D/DGS-1024D.ino
+++ DGS-1024D/DGS-1024D.ino
@@ -31,6 +31,20 @@
 #define dataPin3e4 10 // pin 11 DGS-1024D DS3
 #define dataPin1e2 12 // pin 15 DGS-1024D DS1
 
+/*
+// Pin connected to 74HC164 led board new config better
+// with original cable and Arduino Mini.
+// Original cable has a GND wire between signals and has
+// better crosstalk performance, you can decrease the delay!
+#define clockPin5e6 4 // pin 5 DGS-1024D CP5E6
+#define clockPin3e4 6 // pin 9 DGS-1024D CP3E4
+#define clockPin1e2 8 // pin 13 DGS-1024D CP1E2
+////Pin connected to DS of 74HC164
+#define dataPin5e6 5 // pin 7 DGS-1024D DS5
+#define dataPin3e4 7 // pin 11 DGS-1024D DS3
+#define dataPin1e2 9 // pin 15 DGS-1024D DS1
+*/
+
 //#define DEBUG
 
 void cancella(unsigned char dataPin, unsigned char clockPin) {
